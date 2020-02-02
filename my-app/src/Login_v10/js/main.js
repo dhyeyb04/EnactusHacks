29aaaa6ab0267d93712193dc31e0eb131b2655b1
Dhyey Bhavsar <dhyeybhavsar2000@gmail.com>
2020-02-01 19:48:17 -0500

    Formatted login page, still have to connect to main

--- my-app/src/Login_v10/js/main.js
+++ my-app/src/Login_v10/js/main.js
@@ -1,8 +1,5 @@
-
 (function ($) {
     "use strict";
-
-
     /*==================================================================
     [ Validate ]*/
     var input = $('.validate-input .input100');
@@ -16,10 +13,8 @@
                 check=false;
             }
         }
-
         return check;
     });
-
 
     $('.validate-form .input100').each(function(){
         $(this).focus(function(){
@@ -31,6 +26,8 @@
         if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
             if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
                 return false;
+            } else {
+              window.open('../index.html');
             }
         }
         else {
@@ -42,16 +39,11 @@
 
     function showValidate(input) {
         var thisAlert = $(input).parent();
-
         $(thisAlert).addClass('alert-validate');
     }
 
     function hideValidate(input) {
         var thisAlert = $(input).parent();
-
         $(thisAlert).removeClass('alert-validate');
     }
-    
-    
-
 })(jQuery);
